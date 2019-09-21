7eb13291356f880550c87c19cb56a46a24031290
imotok23 <imotok@gmail.com>
2019-09-21 23:37:39 +0900

    node.jsのテストコード

---
+++ test1.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server = http.createServer
+	(function (request, response) {
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.end("Hello World\n");
+});
+
+server.listen(9000);
+
+console.log("Server running at http://127.0.0.1:9000/");
+
